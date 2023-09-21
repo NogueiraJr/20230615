dd55903498dab96458d6885b732b0166e431b7ca
Nogueira Jr <nogueirajr@hotmail.com>
2023-09-21 10:22:38 -0300

    Alterando ref da rota user.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,11 @@ import { FastifyInstance, RouteOptions } from 'fastify';
 import { getUsersHandler, getUserHandler, createUserHandler, updateUserHandler, deleteUserHandler } from '../controllers/userController';
 
 const userRoutes = (router: FastifyInstance) => {
-  router.get('/users', getUsersHandler);
-  router.get('/users/:id', getUserHandler);
-  router.post('/users', createUserHandler);
-  router.patch('/users/:id', updateUserHandler);
-  router.delete('/users/:id', deleteUserHandler);
+  router.get('/user', getUsersHandler);
+  router.get('/user/:id', getUserHandler);
+  router.post('/user', createUserHandler);
+  router.patch('/user/:id', updateUserHandler);
+  router.delete('/user/:id', deleteUserHandler);
 };
 
 export default userRoutes;
