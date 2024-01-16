d3acaa673e27f3d073d23295f1eed19d555fc65a
Nogueira Jr <nogueirajr@hotmail.com>
2024-01-16 19:22:41 -0300

    Return Users Summary

--- src/services/user/getUsers.ts
+++ src/services/user/getUsers.ts
@@ -2,7 +2,24 @@ import { _getUsers } from '../../repository/user/getUsers';
 
 export async function getUsers() {
   try {
-    return await _getUsers();
+    const users = await _getUsers();
+    const userSummary = users.map((user) => ({
+      name: user.name,
+      userType: {
+        name: user.userType.name,
+        description: user.userType.description,
+      },
+      active: user.active,
+      emails: user.emails.map((email) => ({
+        email: email.email,
+        type: email.userEmailTypeId,
+      })),
+      phones: user.phones.map((phone) => ({
+        phone: phone.phone,
+        type: phone.userPhoneTypeId,
+      })),
+    }));
+    return userSummary;
   } catch (error) {
     throw error;
   } finally {
