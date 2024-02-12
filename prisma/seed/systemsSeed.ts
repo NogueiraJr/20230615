3226570c4dcc8bc306fb2488851f8e2bfec20a1e
Nogueira Jr <nogueirajr@hotmail.com>
2024-02-12 16:59:39 -0300

    Refactory Systems Seed.

--- prisma/seed/systemsSeed.ts
+++ prisma/seed/systemsSeed.ts
@@ -3,17 +3,25 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const systemsData = [
-  { id: 'sysOCSTApp',            seq: '0000', name: 'OCSTApp 1.0', description: 'Sistema Administrador do Aplicativo', },
-  { id: 'sysLocacaoRoupa',       seq: '0110', name: 'Locação de Roupas 1.0', description: 'Reservas, Retiradas e Devoluções', },
-  { id: 'sysLocacaoCarros',      seq: '0120', name: 'Locação de Carros 1.0', description: 'Reservas, Locações, Devoluções', },
-  { id: 'sysOficinaCarros',      seq: '0210', name: 'Oficina de Carros 1.0', description: 'Agendamentos de Serviços, Vendas de Kits', },
-  { id: 'sysPetShop',            seq: '0310', name: 'PetShop 1.0', description: 'Agendamentos, Atendimentos, Orçamentos, Ficha de Atendimento', },
-  { id: 'sysClinicaVeterinaria', seq: '0320', name: 'Clínica Veterinária', description: 'Agendamentos, Orçamentos, Consultas', },
-  { id: 'sysCabeleireiro',       seq: '0410', name: 'Cabeleireiro 1.0', description: 'Agendamento de Horários para Atendimentos, Serviços', },
-  { id: 'sysClinicaEstetica',    seq: '0420', name: 'Clínica Estética 1.0', description: 'Agendamentos, Orçamentos, Consultas', },
-  { id: 'sysDentista',           seq: '0510', name: 'Dentista 1.0', description: 'Agendamento de Consultas, Orçamentos, Atendimentos, Histórico de Pacientes', },
+  //Admin
+  { id: 'sysOCSTApp',            seq: '0000', name: 'OCSTApp 1.0',             description: 'Sistema Administrador do Aplicativo', },
+  //Locações
+  { id: 'sysLocacaoRoupa',       seq: '0110', name: 'Locação de Roupas 1.0',   description: 'Reservas, Retiradas e Devoluções', },
+  { id: 'sysLocacaoCarros',      seq: '0120', name: 'Locação de Carros 1.0',   description: 'Reservas, Locações, Devoluções', },
+  //Oficina
+  { id: 'sysOficinaCarros',      seq: '0210', name: 'Oficina de Carros 1.0',   description: 'Agendamentos de Serviços, Vendas de Kits', },
+  //Pet
+  { id: 'sysPetShop',            seq: '0310', name: 'PetShop 1.0',             description: 'Agendamentos, Atendimentos, Orçamentos, Ficha de Atendimento', },
+  { id: 'sysClinicaVeterinaria', seq: '0320', name: 'Clínica Veterinária 1.0', description: 'Agendamentos, Orçamentos, Consultas', },
+  //Estética
+  { id: 'sysCabeleireiro',       seq: '0410', name: 'Cabeleireiro 1.0',        description: 'Agendamento de Horários para Atendimentos, Serviços', },
+  { id: 'sysClinicaEstetica',    seq: '0420', name: 'Clínica Estética 1.0',    description: 'Agendamentos, Orçamentos, Consultas', },
+  //Saúde
+  { id: 'sysDentista',           seq: '0510', name: 'Dentista 1.0',            description: 'Agendamento de Consultas, Orçamentos, Atendimentos, Histórico de Pacientes', },
+  //Eventos
   { id: 'sysEstudioFotografico', seq: '0610', name: 'Estúdio Fotográfico 1.0', description: 'Orçamentos, Clientes, Amostras, Agendamentos', },
-  { id: 'sysLanchonete',         seq: '0710', name: 'Lanchonete 1.0', description: 'Pedidos e Ordens de Serviços para a Cozinha', },
+  //Alimentação
+  { id: 'sysLanchonete',         seq: '0710', name: 'Lanchonete 1.0',          description: 'Pedidos e Ordens de Serviços para a Cozinha', },
 ];
 
 async function main() {
