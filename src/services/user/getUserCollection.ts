76554e4a113a3974117ed9ab2fac9b24415f2224
Nogueira Jr <nogueirajr@hotmail.com>
2024-02-12 19:28:52 -0300

    Hiding some information when querying user collections.

--- src/services/user/getUserCollection.ts
+++ src/services/user/getUserCollection.ts
@@ -14,14 +14,72 @@ export async function getUserCollection(id: string) {
     function mapCollectionUsers(user: any): any[] {
       const subUsers = collectionUsers.filter((subUser: any) => subUser.userId === user.id);
       return subUsers.map((subUser: any) => ({
-        ...subUser,
+        id: subUser.id,
+        name: subUser.name,
+        usr: subUser.usr,
+        active: subUser.active,
+        emails: subUser.emails.map((email: any) => ({
+          id: email.id,
+          email: email.email,
+          userEmailTypeId: email.userEmailTypeId,
+          active: email.active,
+        })),
+        phones: subUser.phones.map((phone: any) => ({
+          id: phone.id,
+          phone: phone.phone,
+          userPhoneTypeId: phone.userPhoneTypeId,
+          active: phone.active,
+        })),
+        userSystemMenuModule: subUser.userSystemMenuModule.map((userSystemMenuModule: any) => ( {
+          id: userSystemMenuModule.id,
+          active: userSystemMenuModule.active,
+          name: userSystemMenuModule.systemMenuModuleId,
+        })),
+        userType: {
+          id: subUser.userType.id,
+          name: subUser.userType.name,
+          description: subUser.userType.description,
+          active: subUser.userType.active,
+        },
         subUsers: mapCollectionUsers(subUser), // Chama a função recursivamente para mapear os subusuários dos subusuários
       }));
     }
 
     mainUser.subUsers = mapCollectionUsers(mainUser);
 
-    return mainUser;
+    // Filter the main user fields
+    const filteredMainUser = {
+      id: mainUser.id,
+      name: mainUser.name,
+      usr: mainUser.usr,
+      active: mainUser.active,
+      emails: mainUser.emails.map((email: any) => ({
+        id: email.id,
+        email: email.email,
+        userEmailTypeId: email.userEmailTypeId,
+        active: email.active,
+      })),
+      phones: mainUser.phones.map((phone: any) => ({
+        id: phone.id,
+        phone: phone.phone,
+        userPhoneTypeId: phone.userPhoneTypeId,
+        active: phone.active,
+      })),
+      userSystemMenuModule: mainUser.userSystemMenuModule.map((userSystemMenuModule: any) => ( {
+        id: userSystemMenuModule.id,
+        active: userSystemMenuModule.active,
+        name: userSystemMenuModule.systemMenuModuleId,
+      })),
+    userType: {
+        id: mainUser.userType.id,
+        name: mainUser.userType.name,
+        description: mainUser.userType.description,
+        active: mainUser.userType.active,
+      },
+      subUsers: mainUser.subUsers,
+    };
+
+    return filteredMainUser;
   } catch (error) {
     throw error;
   }
