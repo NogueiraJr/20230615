f0407f0cad273b2c1fbc1c32b1dbec862f152e03
Nogueira Jr <nogueirajr@hotmail.com>
2024-04-14 18:36:17 -0300

    Login Modularization

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,20 +1,12 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { getUserByLogin } from "../services/user/getUserByLogin";
-import { UserPayload } from '../repository/interface/UserPayload';
 import { errorHandler } from '../errors/errorHandler';
 
 const bcrypt = require('bcryptjs');
 
 export const getUserLoginHandler = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
-    const { psw } = request.body as UserPayload;
-    const user = await getUserByLogin(request);
-
-    if (user.id == undefined) return reply.status(400).send({ message: 'Usuário não encontrado' });
-
-    const validPassword = await bcrypt.compare(psw, user.psw);
-    if (!validPassword) return reply.status(400).send({ message: 'Credenciais inválidas' });
-
+    const user = await getUserByLogin(request, reply);
     reply.status(200).send(user);
   } catch (error) {
     errorHandler(error as Error, reply);
