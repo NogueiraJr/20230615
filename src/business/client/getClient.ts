dd1430391e7a62917f71230fcf910ab1c2520df8
Nogueira Jr <nogueirajr@hotmail.com>
2024-07-23 20:25:32 -0300

    Refactory Get Client

--- src/business/client/getClient.ts
+++ src/business/client/getClient.ts
@@ -1,8 +1,11 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { _getClientByName } from "../../repository/client/getClient";
+import { IGetClientBody } from "../../interfaces/request/IGetClientBody";
 
-export async function getClient(name: string, reply: FastifyReply) {
+export async function getClient(request: FastifyRequest<{ Body: IGetClientBody }>, reply: FastifyReply) {
   try {
+    const { name } = request.body;
+
     if (name == undefined) return reply.status(400).send({ message: 'Nome necessário' });
     
     const client = await _getClientByName(name);
--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,11 +1,11 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { getClient } from "../business/client/getClient";
 import { errorHandler } from '../errors/errorHandler';
+import { IGetClientBody } from '../interfaces/request/IGetClientBody';
 
-export const getClientHandler = async (request: FastifyRequest<{ Params: {name: string } }>, reply: FastifyReply) => {
+export const getClientHandler = async (request: FastifyRequest<{ Body: IGetClientBody }>, reply: FastifyReply) => {
   try {
-    const { name } = request.params;
-    const client = await getClient(name, reply);
+    const client = await getClient(request, reply);
     reply.status(200).send(client);
   } catch (error) {
     errorHandler(error as Error, reply);
